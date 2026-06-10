0ea58a92d5150202509b378aa87abe539b7f8314
GrovitAI <team@grovitai.com>
2026-06-10 16:42:14 +0530

    task 42: set video loader contentFit to contain for center alignment

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1710,7 +1710,7 @@ export default function PosBillingScreen() {
             <VideoView
               style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
               player={videoPlayer}
-              contentFit="cover"
+              contentFit="contain"
               nativeControls={false}
             />
 
